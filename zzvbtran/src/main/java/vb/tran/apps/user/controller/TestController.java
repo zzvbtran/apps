1e1667e788b33e7510b50ef6652a145dbbeadb63
tqhuy <quochuy91.np@gmail.com>
2018-09-09 14:16:29 +0700

    donete

--- zzvbtran/src/main/java/vb/tran/apps/user/controller/TestController.java
+++ zzvbtran/src/main/java/vb/tran/apps/user/controller/TestController.java
@@ -1,19 +1,18 @@
 package vb.tran.apps.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
 
-	private String message;
-	
 	@RequestMapping(value= {"/", "/index"},method=RequestMethod.GET)
-	public String index() {
-		///ddddeddd
-		// 002
-		// 003
-		return "xxx";
+	public String index(Model model) {
+		  String message = "Hello Spring Boot + JSP";
+		  
+	        model.addAttribute("message", message);
+		return "index";
 	}
 }
